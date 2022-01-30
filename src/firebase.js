4a7d213b1d935e3cde4ab88737ed98877f7776d6
only1deepak <53404785+only1deepak@users.noreply.github.com>
2022-01-30 17:44:47 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClIxa6ePpvI5SEnDO6ezQ8UwYis8pJWos",
+    authDomain: "z-6ea12.firebaseapp.com",
+    projectId: "z-6ea12",
+    storageBucket: "z-6ea12.appspot.com",
+    messagingSenderId: "886190254806",
+    appId: "1:886190254806:web:a07ae03423bb8cc0fb7777",
+    measurementId: "G-1QJZK4V4KH"
+  };
