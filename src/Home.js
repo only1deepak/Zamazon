e4970e8995cee4615646758d596272455a7c4fb0
only1deepak <53404785+only1deepak@users.noreply.github.com>
2022-02-06 01:07:07 +0530

    add checkout page

--- src/Home.js
+++ src/Home.js
@@ -12,18 +12,61 @@ function Home() {
           alt=""
         ></img>
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            id="12345"
+            title="Sapiens A Graphic History, Volume 1: The Birth of Humankind"
+            price={299.0}
+            image="https://m.media-amazon.com/images/P/B08FR41GS8.01._SCLZZZZZZZ_SX500_.jpg"
+            rating={4}
+          />
+          <Product
+            id="92423"
+            title="Mamaearth Anti Hair Fall Spa Range"
+            price={856.0}
+            image="https://m.media-amazon.com/images/I/61mNnwQnmbL._SL1200_.jpg"
+            rating={4}
+          />
+          <Product
+            id="87563"
+            title="GIVA 925 Sterling Silver Zircon Cubic Zirconia Vintage Adjustable Ring"
+            price={5550.0}
+            image="https://m.media-amazon.com/images/I/71FTGCh6gnL._UL1500_.jpg"
+            rating={4}
+          />
+          <Product
+            id="12356"
+            title="pTron Bassbuds Jade Gaming True Wireless Headphone with 40Hrs Total Playtime"
+            price={1499.0}
+            image="https://m.media-amazon.com/images/I/51tBu12hIFL._SL1100_.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="35849"
+            title="OnePlus Nord 2 5G (Blue Haze, 8GB RAM, 128GB Storage)"
+            price={29999.0}
+            image="https://m.media-amazon.com/images/I/61TnX0PmqES._SL1500_.jpg"
+            rating={4}
+          />
+          <Product
+            id="92485"
+            title="BAGHADBILLO Unisex Stylish Latest Printed Pullover Cotton Hoodies Sweatshirt for Men and Women"
+            price={701.0}
+            image="https://m.media-amazon.com/images/I/51ZIqCyzpbL._UL1000_.jpg"
+            rating={3}
+          />
         </div>
 
         <div className="home__row">
-          <Product />
+          <Product
+            id="98565"
+            title="MSI GF75 Intel i7-10750H 17.3 inches FHD IPS-Level 144Hz Panel Thin Gaming Laptop (16GB/512GB NVMe SSD/Windows 10 Home/Nvidia RTX3050 Ti 4GB GDDR6/Black/2.2Kg), 10UD-086IN"
+            price={91990.0}
+            image="https://m.media-amazon.com/images/I/71j3-8OXvYL._SL1500_.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,28 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, price, image, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The Lean Startup</p>
+        <p>{title}</p>
 
         <p className="product__price">
           <small>₹</small>
-          <strong>199.00</strong>
+          <strong>{price}</strong>
         </p>
 
         <div className="product__rating">
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
 
       <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+        src={image}
         alt=""
       ></img>
       <button>Add to Basket</button>
